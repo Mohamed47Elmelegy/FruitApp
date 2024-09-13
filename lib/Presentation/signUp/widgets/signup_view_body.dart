fdf1e4cbf19406ea957ef25d161ce6167097d616
MohamedElmelegy74 <75188929+MohamedElmelegy74@users.noreply.github.com>
2024-09-13 15:33:32 +0300

    SignupView and vaidate errors input

--- lib/Presentation/signUp/widgets/signup_view_body.dart
+++ lib/Presentation/signUp/widgets/signup_view_body.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:frutes_app/core/extensions/padding_ext.dart';
+import 'package:frutes_app/core/routes/page_routes_name.dart';
+import 'package:frutes_app/main.dart';
+import '../../../core/widgets/custom_app_bar.dart';
+import '../view/signup_view.dart';
 
-class SignupViewBody extends StatefulWidget {
+class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
-  State<SignupViewBody> createState() => _SignupViewBodyState();
-}
-
-class _SignupViewBodyState extends State<SignupViewBody> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: appBar(context,
+          title: 'حساب جديد',
+          onTap: () => navigatorKey.currentState
+              ?.pushReplacementNamed(PageRoutesName.login)),
+      body: const SignupView()
+          .setHorizontalPadding(context, 16, enableMediaQuery: false),
+    );
   }
 }
